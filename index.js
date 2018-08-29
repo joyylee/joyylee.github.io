7e39dad992aec3784d5e30c18e30d508eedc3d68
joyylee <jlee9623@gmail.com>
2018-08-29 13:58:50 -0400

    organizing projects

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("joyceLee1");
-var header2 = document.getElementById("joyceLee2");
+// var header2 = document.getElementById("joyceLee2");
+var nav = document.getElementById("nav");
 
 var sticky = header.offsetTop;
-var sticky = header2.offsetTop;
+var sticky2 = header2.offsetTop;
+var sticky3 = nav.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     header.classList.add("sticky");
+    nav.classList.add("stickys2");
   } else {
     header.classList.remove("sticky");
-  }
-
-  if (window.pageYOffset > sticky2) {
-    header2.classList.add("stickys");
-  } else {
-    header2.classList.remove("stickys");
+    nav.classList.remove("stickys2");
   }
 
 }
