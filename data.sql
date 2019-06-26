6a177bedbb81cee2429dd26236c35f7274d7d2d6
J Lee <jlee@Js-MacBook-Air.local>
2019-06-25 19:39:06 -0700

    uhhh added database and added to database

---
+++ data.sql
@@ -0,0 +1,49 @@
+--sqlite3 -batch -line var/insta485.sqlite3 'SELECT * FROM comments'
+INSERT INTO arts(title, filename, caption)
+VALUES (Magnolia , "img/magnolia.jpg", 'Graphite, Colored Pencil');
+
+INSERT INTO arts(title, filename, caption)
+VALUES (Carnations , "img/c.jpg", 'Graphite, Watercolor');
+
+INSERT INTO arts(title, filename, caption)
+VALUES (Iris , "img/iris.jpg", 'Graphite, Watercolor');
+
+INSERT INTO arts(title, filename, caption)
+VALUES (Please, could you be tender  , "img/jk.jpg", 'Graphite, digitally colored');
+
+INSERT INTO arts(title, filename, caption)
+VALUES (Sun , "img/orange.jpg", 'Colored Pencil');
+
+
+INSERT INTO designs(title, filename, caption)
+VALUES (quattourEbene , "img/quattourEbene.jpg", 'Poster mock up for an imaginary musical show. Tried to capture the characteristics of the
+                musical group.');
+
+INSERT INTO designs(title, filename, caption)
+VALUES (mentorshipEvent , "img/mentorshipEvent.jpg", 'Poster designed for the KSEA mentorship event with the KSEA michigan graduate chapter. ');
+
+INSERT INTO designs(title, filename, caption)
+VALUES (springrolls , "img/springrolls.jpg", 'Poster designed for the KSEA social event.');
+
+INSERT INTO designs(title, filename, caption)
+VALUES (crackFinals , "img/crackFinals.jpg", 'Flyer designed for the KSEA mass meeting.');
+
+INSERT INTO designs(title, filename, caption)
+VALUES (party , "img/party.jpg", 'Apparel designed for KSEA board members.');
+
+INSERT INTO designs(title, filename, caption)
+VALUES (KSEAapparel , "img/KSEAapparel.jpg", 'Personal logo designed in 2D Design class in illustrator. Laser cut into stickers.');
+
+INSERT INTO projects(title, filename, caption)
+VALUES (eat2 , "img/eat2.jpg", 'Eat is an iOS application that produces a list of potential recipes based on a list of
+            ingredients
+            the user inputs into the app. Filled with our personal favorite recipes. Developed in Swift and designed by
+            college students, for college students. ');
+
+INSERT INTO projects(title, filename, caption)
+VALUES (f , "img/f.jpg", 'Partnered with a rural house museum to construct a portable, candle-shaped object that uses
+            RFID
+            technology to add an interactive aspect to the museum. The object was designed based on the needs of the
+            museum, including limited
+            electricity, ease of use, portability, and the ability for the user to guide themselves through the museum
+            without a tour guide. Currently being used in the museum!');
